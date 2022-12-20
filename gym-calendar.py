bf0b376ebe25e84e66a5cc626d70dcffe202bf01
Rasan Rasch <rasan@nyu.edu>
2022-12-20 11:34:03 -0500

    Print multiple checkins on the same date

--- gym-calendar.py
+++ gym-calendar.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
 
+from collections import defaultdict
 from datetime import datetime, timedelta
 from dateutil import tz
 from dateutil.parser import parse
 from icalendar import Calendar, Event
 from pathlib import Path
+from pprint import pprint
 import pytz
 import re
 
 location_file = "location.txt"
-checkins_file = "checkins.txt"
+checkins_file = "checkins-fixed.txt"
 
 def is_dst(dt, timeZone):
     aware_dt = timeZone.localize(dt)
@@ -34,6 +36,8 @@ line_iter = iter(lines)
 
 local_timezone = pytz.timezone("US/Eastern")
 
+dates = defaultdict(list)
+
 for line in line_iter:
     date_match = re.search(r"^(\d+/\d+)", line)
     if date_match:
@@ -43,12 +47,14 @@ for line in line_iter:
         time = re.sub(r":/", ":7", time)
         time_match = re.search("^(\d+):(\d+)([ap]m)", time)
         if not time_match:
-            raise ValueError(f"Invalid time {time}")
+            raise ValueError(f"Invalid time {date} {time}")
         hour = time_match.group(1)
         minutes = int(time_match.group(2))
         ampm = time_match.group(3)
         full_date = f"{date}/2022 {hour}:{minutes:02d}{ampm}"
-        # print(full_date)
+        print(full_date)
+
+        dates[date].append(time)
 
         start_time = parse(full_date)
         # start_time = start_time.replace(tzinfo=tz.tzlocal())
@@ -59,7 +65,7 @@ for line in line_iter:
         # print(start_time.astimezone().strftime('%Y-%m-%d %I:%M %p %Z%z'))
         # print(end_time.astimezone().strftime('%Y-%m-%d %I:%M %p %Z%z'))
 
-        uid = start_time.strftime("%Y%m%d%I%M%p%Z") + "@GymCalendar"
+        uid = start_time.strftime("%Y%m%d%I%M%p%Z") + "@MyGymCalendar"
 
         event = Event()
         event.add("summary", "Gym Workout")
@@ -69,6 +75,11 @@ for line in line_iter:
         event.add("location", location)
         event["uid"] = uid
         cal.add_component(event)
+
+print("Dates with multiple checkins:")
+for date, times in dates.items():
+    if len(times) > 1:
+        print(f"{date}: {times}")
 
 with open("calendar.ics", "wb") as fh:
     fh.write(cal.to_ical())
