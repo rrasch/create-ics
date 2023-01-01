87b9446d3427766ae6eebc211292a5521b2088e4
Rasan Rasch <rasan@nyu.edu>
2023-01-01 09:02:35 -0500

    Geocode address and read pdf from cmdline

--- club-visits-cal.py
+++ club-visits-cal.py
@@ -2,13 +2,23 @@
 
 from datetime import timedelta
 from dateutil.parser import parse
+from geopy.geocoders import GoogleV3
 from icalendar import Calendar, Event
+import argparse
+import os
 import pandas as pd
 import pytz
 import re
 import tabula
 
-file = "Club-Visits-20211230-20221230.pdf"
+parser = argparse.ArgumentParser(
+    description="Create calendar for fitness club checkins.")
+parser.add_argument("pdf_file", metavar="CHECKIN_PDF_FILE",
+    help="pdf file containing checkin times")
+args = parser.parse_args()
+
+maps_api_key = os.environ.get("MAPS_API_KEY")
+geolocator = GoogleV3(api_key=maps_api_key)
 
 local_timezone = pytz.timezone("US/Eastern")
 
@@ -20,7 +30,7 @@ pd.set_option(
 
 pdopt = {"header": None}
 
-tables = tabula.read_pdf(file, pages="all", pandas_options=pdopt)
+tables = tabula.read_pdf(args.pdf_file, pages="all", pandas_options=pdopt)
 
 tables[0].drop(index=0, inplace=True)
 
@@ -28,12 +38,19 @@ cal = Calendar()
 cal.add("prodid", f"-//rrasch/ClubCheckinCalendar//EN")
 cal.add("version", "2.0")
 
+cleaned_address = {}
+
 for df in tables:
     for index, row in df.iterrows():
         checkin_time, club, address = row
 
         club = club.replace("\r", " ")
         club = re.sub(r" SS$", " Super Sport", club)
+
+        if address not in cleaned_address:
+            cleaned_address[address] = geolocator.geocode(address)
+
+        location = f"{club}, {cleaned_address[address]}"
 
         start_time = parse(checkin_time)
         start_time = local_timezone.localize(start_time)
@@ -46,7 +63,7 @@ for df in tables:
         event.add("description", f"Gym Workout {checkin_time}")
         event.add("dtstart", start_time)
         event.add("dtend", end_time)
-        event.add("location", f"{club}, {address}")
+        event.add("location", location)
         event["uid"] = uid
         cal.add_component(event)
 
